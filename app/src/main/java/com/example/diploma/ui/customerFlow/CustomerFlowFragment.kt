41597c3294ddbaec5305d49aa0c939af6501be7f
anako <anakovaleva026@gmail.com>
2024-05-19 14:05:47 +0300

    Оформление фрагментов в лучшем виде :D

--- app/src/main/java/com/example/diploma/ui/customerFlow/CustomerFlowFragment.kt
+++ app/src/main/java/com/example/diploma/ui/customerFlow/CustomerFlowFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.diploma.databinding.FragmentCustomerFlowBinding
@@ -25,10 +24,6 @@ class CustomerFlowFragment : Fragment() {
         _binding = FragmentCustomerFlowBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
-        customerFlowViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
